03246e49bbb8fde3f9797836d57de41e3f844d77
torgard <aksel@akseltorgard.com>
2016-09-21 03:25:33 +0200

    Added bounds checking to ImageHandler.updateImage

--- src/main/java/heatmap/ImageHandler.java
+++ src/main/java/heatmap/ImageHandler.java
@@ -28,62 +28,54 @@ public class ImageHandler {
 
         int area = 32;
 
+        int w = img.getWidth();
+        int h = img.getHeight();
+
+        //Check bounds
+        if (gX < 0 || gX > w || gY < 0 || gY > h) {
+            return false;
+        }
+
         for (int x = -area; x <= area; x++) {
+
+            if (x < 0) {
+                continue;
+            }
+
             for (int y = -area; y <= area; y++) {
-                try {
-                    int color = img.getRGB(gX+x, gY+y);
 
-                    int r = (color & 0xff0000) >>> 16;
-                    int g = (color & 0x00ff00) >>> 8;
-                    int b = color & 0x0000ff;
-
-                    float[] hsb = Color.RGBtoHSB(r,g,b, null);
-
-                    //System.out.println(hsb[0] + ", " + hsb[1] + ", " + hsb[2]);
-
-                    if (hsb[0] <= 0.004575163f) {
-                        continue;
-                    }
-
-                    else if(hsb[0] >= 0.666 && hsb[2] < 1f) {
-                        hsb[2] += 0.01f;
-                    }
-
-                    else {
-                        hsb[0] -= 0.005f;
-                        //hsb[2] = 1f;
-                    }
-
-                    hsb[1] = 1f;
-
-                    color = Color.HSBtoRGB(hsb[0], hsb[1], hsb[2]);
-
-                    img.setRGB(gX+x, gY+y, color);
-
-                    /*
-                    int a = color & 0xff000000;
-
-                    color = color & 0xffffff;
-
-                    if (color == 0xff0000) {
-                        continue;
-                    }
-
-                    color <<= 1;
-
-                    if (color < 0xff) {
-                        color |= 1;
-                    }
-
-                    int intensity = 255 - (Math.abs(x) * Math.abs(y));
-
-                    color |= a | (intensity << 24);
-                    */
-
-                    img.setRGB(gX+x, gY+y, color);
-                } catch (Exception e) {
-                    //System.err.println(e.getMessage());
+                if (y < 0) {
+                    continue;
                 }
+
+                int color = img.getRGB(gX+x, gY+y);
+
+                int r = (color & 0xff0000) >>> 16;
+                int g = (color & 0x00ff00) >>> 8;
+                int b = color & 0x0000ff;
+
+                float[] hsb = Color.RGBtoHSB(r,g,b, null);
+
+                //System.out.println(hsb[0] + ", " + hsb[1] + ", " + hsb[2]);
+
+                if (hsb[0] <= 0.004575163f) {
+                    continue;
+                }
+
+                else if(hsb[0] >= 0.666 && hsb[2] < 1f) {
+                    hsb[2] += 0.01f;
+                }
+
+                else {
+                    hsb[0] -= 0.005f;
+                    //hsb[2] = 1f;
+                }
+
+                hsb[1] = 1f;
+
+                color = Color.HSBtoRGB(hsb[0], hsb[1], hsb[2]);
+
+                img.setRGB(gX+x, gY+y, color);
             }
         }
 
