ee405b276e56a8093f3a3fa12ba7b35ea87cd978
torgard <aksel@akseltorgard.com>
2016-09-23 06:11:54 +0200

    Added unit tests for BufferedHeatmapImage.updatePixel(x,y).

--- src/test/java/heatmap/BufferedHeatmapImageTest.java
+++ src/test/java/heatmap/BufferedHeatmapImageTest.java
@@ -1,11 +1,43 @@
 package heatmap;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BufferedHeatmapImageTest {
 
-    @Test
-    public void testUpdatePixel() {
+    public BufferedHeatmapImage getOneByOneImage() {
+        int w = 1;
+        int h = 1;
 
+        return new BufferedHeatmapImage(w,h);
+    }
+
+    @Test
+    public void testInitialColorIsTransparentBlue() {
+        BufferedHeatmapImage img = getOneByOneImage();
+
+        int rgb = img.getRGB(0,0);
+
+        Assert.assertEquals(rgb, 0x000000FF);
+    }
+
+    @Test
+    public void testAlphaIncreasesCorrectly() {
+        BufferedHeatmapImage img = getOneByOneImage();
+
+        img.updatePixel(0,0);
+
+        int rgb = img.getRGB(0,0);
+
+        Assert.assertEquals(rgb, 0x010000FF);
+
+        for(int i = 1; i < 255; i++) {
+            img.updatePixel(0,0);
+
+        }
+
+        rgb = img.getRGB(0,0);
+
+        Assert.assertEquals(rgb, 0xFF0000FF);
     }
 }
