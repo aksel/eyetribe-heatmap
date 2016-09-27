fb5eee1609ef03284f778618354403c42089c3bc
torgard <aksel@akseltorgard.com>
2016-09-27 19:53:10 +0200

    Added context menu to HeatmapFrame, so that it is easies to close it in its undecorated state.

--- src/main/java/heatmap/HeatmapFrame.java
+++ src/main/java/heatmap/HeatmapFrame.java
@@ -2,14 +2,19 @@ package heatmap;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 
 public class HeatmapFrame extends JFrame {
+
+    private JPopupMenu contextMenu;
 
     public HeatmapFrame(final BufferedImage img) {
         setUndecorated(true);
         setLayout(new BorderLayout());
         setSize(new Dimension(img.getWidth(), img.getHeight()));
+
+        createContextMenu();
 
         JPanel imagePanel = new JPanel() {
             @Override
@@ -33,5 +38,37 @@ public class HeatmapFrame extends JFrame {
 
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private void createContextMenu() {
+        contextMenu = new JPopupMenu();
+
+        JMenuItem menuItem = new JMenuItem("Close");
+        menuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispatchEvent(new WindowEvent(HeatmapFrame.this, WindowEvent.WINDOW_CLOSING));
+            }
+        });
+
+        contextMenu.add(menuItem);
+
+        addMouseListener(new PopupListener());
+    }
+
+    class PopupListener extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            showContextMenu(e);
+        }
+
+        public void mouseReleased(MouseEvent e) {
+            showContextMenu(e);
+        }
+
+        private void showContextMenu(MouseEvent e) {
+            if (e.isPopupTrigger()) {
+                contextMenu.show(e.getComponent(),
+                        e.getX(), e.getY());
+            }
+        }
     }
 }
